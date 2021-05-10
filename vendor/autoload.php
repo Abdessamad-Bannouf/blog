d01d094a1da55b702b1cc84f1253240b232c846f
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-11 00:09:47 +0200

    autoload

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,5 +4,5 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit6359b5da2a02e6ea2be765e496da3293::getLoader();
+return ComposerAutoloaderInit6359b5da2a02e6ea2be765e496da3293::getLoader(); 
    

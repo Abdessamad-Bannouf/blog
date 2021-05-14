328fab7573190df04c5622a1e36182733f429a07
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-14 09:14:04 +0200

    Vendor

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -11,8 +11,8 @@ return array(
     'App\\Classes\\Form\\Validate' => $baseDir . '/App/Classes/Form/Validate.php',
     'App\\Classes\\Regex\\Regex' => $baseDir . '/App/Classes/Regex/Regex.php',
     'App\\Classes\\Session\\Session' => $baseDir . '/App/Classes/Session/Session.php',
+    'App\\Controller\\Controller' => $baseDir . '/App/Controller/Controller.php',
     'App\\Controller\\UserController' => $baseDir . '/App/Controller/userController.php',
-    'App\\Controllers\\Controller' => $baseDir . '/App/Controller/Controller.php',
-    'App\\Models\\Model' => $baseDir . '/App/Model/Model.php',
-    'App\\Models\\UserModel' => $baseDir . '/App/Model/userModel.php',
+    'App\\Model\\Model' => $baseDir . '/App/Model/Model.php',
+    'App\\Model\\UserModel' => $baseDir . '/App/Model/userModel.php',
 );

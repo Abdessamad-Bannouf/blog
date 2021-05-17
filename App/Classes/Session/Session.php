bd21d0a422964ac31442b04b4bb97510f8f1fb41
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-18 00:04:39 +0200

    Setup session array

--- App/Classes/Session/Session.php
+++ App/Classes/Session/Session.php
@@ -3,22 +3,24 @@
 
     class Session
     {
-        private $Session;
+        private $session;
 
-        public function __construct($Session = array()) // Crée un objet qui va contenir les différentes sessions
+        public function __construct($session = array()) // Crée un objet qui va contenir les différentes sessions
         {
-            $this->Session[] = $Session;
+            $this->session[] = $session;
+            
         }
 
         public function GetSession() // Crée la ou les session(s)
         {
-            for($i=0;$i<count($this->Session);$i++)
-                $this->Session[$i] = $_SESSION[$i];
+            foreach($this->session as $key => $session){
+                $session[$key] = $_SESSION[$key];var_dump($session['lastName']);
+            }
         }
 
         public function DeleteSession() // Détruit la ou les session(s)
         {
-            unset($this->Session);
+            unset($this->session);
         }
     }
 ?>

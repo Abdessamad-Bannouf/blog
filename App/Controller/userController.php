c53b3e78de61b2ca64b23bf326e38451e7526dcd
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-18 00:05:24 +0200

    Show user info

--- App/Controller/userController.php
+++ App/Controller/userController.php
@@ -1,15 +1,25 @@
 <?php 
     namespace App\Controller;
     use App\Controller\Controller;
+    use App\Model\userModel;
+    use App\Classes\Session\Session;
 
-    class UserController extends Controller{
+class UserController extends Controller{
+        private $userModel;
+        private $session;
 
         public function __construct(){
-
+            $this->userModel = new userModel;
+            $this->session = new Session(array("lastName"=>"Bannouf","firstName"=>"Abdessamad"));
+            $this->session->GetSession();
         } 
 
-        public function viewProfil($id = false){
-            echo $id; 
+        public function home(){
+            if(isset($_POST['mail'])){
+
+                $mail = htmlspecialchars($_POST['mail']);
+                $this->userModel->getUser($mail);
+            }
         }
     } 
 ?>

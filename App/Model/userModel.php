9ebb298739548ab0a6e869273a188fc35f35c8e1
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-19 23:58:38 +0200

    Implements subscribeUser method

--- App/Model/userModel.php
+++ App/Model/userModel.php
@@ -6,5 +6,12 @@
         public function getUser($mail){
             $getUserInfo = parent::SelectFilter(array('lastName','firstName'),'user','mail',$mail);
         }
+
+        public function subscribeUser($lastName,$firstName,$mail,$password){
+            $getSubscribe = parent::RequestInsert( 'user',
+                                                    array('lastName','firstName','mail','password','token','isAdmin'),
+                                                    array($lastName,$firstName,$mail,$password,'NULL',0)
+                                                );
+        }
     }
 ?>

4fffd9d1d3d5efb840079e1a1eaca3eeca21e5fd
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-20 00:02:03 +0200

    Modify getSession Method

--- App/Classes/Session/Session.php
+++ App/Classes/Session/Session.php
@@ -3,23 +3,21 @@
 
     class Session
     {
-        private $session;
+        private $name = array();
+        private $value = array();
 
-        public function __construct($session = array()) // Crée un objet qui va contenir les différentes sessions
-        {
-            $this->session[] = $session;
-            
+        public function __construct($name = array(), $value = array()){ // Crée un objet qui va contenir les différentes sessions
+            $this->name = $name;
+            $this->value = $value;
         }
 
-        public function GetSession() // Crée la ou les session(s)
-        {
-            foreach($this->session as $key => $session){
-                $session[$key] = $_SESSION[$key];var_dump($session['lastName']);
-            }
+        public function GetSession(){ // Crée la ou les session(s)
+            for($i=0; $i<count($this->name); $i++){
+                $_SESSION[$this->name[$i]] = $this->value[$i];
+            }     
         }
 
-        public function DeleteSession() // Détruit la ou les session(s)
-        {
+        public function DeleteSession(){ // Détruit la ou les session(s)
             unset($this->session);
         }
     }

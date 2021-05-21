63897bdb50547f255ffe0bfdcd436d7f6609ad88
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-21 11:09:04 +0200

    Fix Session class

--- App/Classes/Session/Session.php
+++ App/Classes/Session/Session.php
@@ -3,22 +3,22 @@
 
     class Session
     {
-        private $Session;
+        private $name = array();
+        private $value = array();
 
-        public function __construct($Session = array()) // Crée un objet qui va contenir les différentes sessions
-        {
-            $this->Session[] = $Session;
+        public function __construct($name = array(), $value = array()){ // Crée un objet qui va contenir les différentes sessions
+            $this->name = $name;
+            $this->value = $value;
         }
 
-        public function GetSession() // Crée la ou les session(s)
-        {
-            for($i=0;$i<count($this->Session);$i++)
-                $this->Session[$i] = $_SESSION[$i];
+        public function GetSession(){ // Crée la ou les session(s)
+            for($i=0; $i<count($this->name); $i++){
+                $_SESSION[$this->name[$i]] = $this->value[$i];
+            }     
         }
 
-        public function DeleteSession() // Détruit la ou les session(s)
-        {
-            unset($this->Session);
+        public function DeleteSession(){ // Détruit la ou les session(s)
+            unset($this->session);
         }
     }
 ?>

885a10fbd99b851c94e3f274fca3a5bb85287504
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-21 10:29:04 +0200

    Remove getUser() method

--- App/Model/userModel.php
+++ App/Model/userModel.php
@@ -3,10 +3,6 @@
     use App\Model\Model; 
 
     class UserModel extends Model{
-        public function getUser($mail,$password){
-            $getUserInfo = parent::SelectFilter(array('mail','password'),'user',array('mail','password'),array($mail,$password));
-        }
-
         public function subscribeUser($lastName,$firstName,$mail,$password){
             $getSubscribe = parent::RequestInsert( 'user',
                                                     array('lastName','firstName','mail','password','token','isAdmin'),

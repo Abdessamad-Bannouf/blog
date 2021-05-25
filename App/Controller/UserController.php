6fc582f34622628f6d578a405467cf6580c94bb2
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-25 17:56:35 +0200

    Add register() method

---
+++ App/Controller/UserController.php
@@ -0,0 +1,42 @@
+<?php 
+    namespace App\Controller;
+    use App\Controller\Controller;
+
+    use App\Model\UserModel;
+
+    use App\Classes\Session\Session;
+
+    use App\Classes\Form\Form;
+
+class UserController extends Controller{
+        private $userModel;
+        private $session;
+        private $form;
+
+        public function __construct(){
+            $this->userModel = new UserModel;
+
+            $this->session = new Session(array("lastName","firstName"),array("Bannouf","Abdessamad"));
+
+            $this->form = new Form;
+
+            $this->session->GetSession();
+        } 
+
+
+        public function register(){
+            if(isset($_POST['mail']) AND isset($_POST['password'])){
+                
+                $mail = htmlspecialchars($_POST['mail']); 
+                $password = htmlspecialchars($_POST['password']);
+                
+                $this->userModel->getRegister($mail,$password);
+
+            }
+
+            parent::Render('App/View/registerView.php',array());
+        }
+        
+        
+    } 
+?>

ece5716ea2a8a0df26c14bf2ef2cf9a7bb604448
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-25 18:14:23 +0200

    Modify register() to addUser()

--- App/Model/UserModel.php
+++ App/Model/UserModel.php
@@ -3,11 +3,9 @@
     use App\Model\Model; 
 
     class UserModel extends Model{
-        public function Register($lastName,$firstName,$mail,$password){
+        public function addUser($lastName,$firstName,$mail,$password){
             $addUser = parent::RequestInsert('user',array('lastName','firstName','mail','password','token','isAdmin'),array($lastName,$firstName,$mail,$password,'NULL',0));
 
-
-            var_dump($addUser);
         }
     }
 ?>

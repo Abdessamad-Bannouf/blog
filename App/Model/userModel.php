fb56573e3b94bd372a3bdaa9b5af1cd318b0e091
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-27 09:24:04 +0200

    remove subscribeUser() method

--- App/Model/userModel.php
+++ App/Model/userModel.php
@@ -3,12 +3,5 @@
     use App\Model\Model; 
 
     class UserModel extends Model{
-        public function subscribeUser($lastName,$firstName,$mail,$password){
-            $getSubscribe = parent::RequestInsert( 'user',
-                                                    array('lastName','firstName','mail','password','token','isAdmin'),
-                                                    array($lastName,$firstName,$mail,$password,'NULL',0)
-                                                );
-                                                var_dump($getSubscribe);
-        }
     }
 ?>

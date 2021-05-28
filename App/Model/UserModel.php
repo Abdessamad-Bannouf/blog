2b3c9387bb3c5929c66ab05f96bd447839bd0d17
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-28 15:27:54 +0200

    Add space to merge dev

--- App/Model/UserModel.php
+++ App/Model/UserModel.php
@@ -3,10 +3,10 @@
     use App\Model\Model; 
 
     class UserModel extends Model{
-        public function addUser($lastName,$firstName,$mail,$password){
-            $addUser = parent::RequestInsert('user',array('lastName','firstName','mail','password','token','isAdmin'),array($lastName,$firstName,$mail,$password,'NULL',0));
-
-            return $addUser;
+        public function addUser($lastName,$firstName,$mail,$password){  
+            $addUser = parent::RequestInsert('user',array('lastName','firstName','mail','password','token','isAdmin'),array($lastName,$firstName,$mail,$password,'NULL',0)); 
+ 
+            return $addUser; 
         }
     }
 ?>

956b05a29a2831392810ff7c689159fc17c87296
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-05-28 15:05:45 +0200

    Re add correctly name file

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -2,22 +2,19 @@
     namespace App\Controller;
     use App\Controller\Controller;
 
-    use App\Model\UserModel;
+    use App\Model\userModel;
 
     use App\Classes\Session\Session;
 
     use App\Classes\Form\Form;
 
-    use App\Classes\Security\Security;
-
 class UserController extends Controller{
         private $userModel;
         private $session;
         private $form;
-        private $security;
 
         public function __construct(){
-            $this->userModel = new UserModel;
+            $this->userModel = new userModel;
 
             $this->session = new Session(array("lastName","firstName"),array("Bannouf","Abdessamad"));
 
@@ -26,45 +23,15 @@ class UserController extends Controller{
             $this->session->GetSession();
         } 
 
+        public function home(){
+            /*if(isset($_POST['mail'])){
 
-        public function login(){
-            if(isset($_POST['mail']) AND isset($_POST['password'])){
-                
-                $mail = htmlspecialchars($_POST['mail']); 
-                $password = htmlspecialchars($_POST['password']);
-                
-                $this->userModel->getUser($mail,$password);
+                $mail = htmlspecialchars($_POST['mail']);
+            }*/
 
-            }
-
-            parent::Render('App/View/loginView.php',array());
-
+            $getUserInfo = $this->userModel->getUserInfo();
             
-        } 
-
-
-        public function register(){
-            $confirmPassword = null;
-            $isRegister = null;
-          
-            $this->security = new Security;
-          
-            if(isset($_POST['lastName']) AND isset($_POST['firstName']) AND isset($_POST['mail']) AND isset($_POST['password']) AND isset($_POST['confirmPassword'])){
-                $lastName = $_POST['lastName'];
-                $firstName = $_POST['firstName'];
-                $mail = htmlspecialchars($_POST['mail']); 
-                $password = htmlspecialchars($_POST['password']);
-                $confirmPassword = isset($_POST['confirmPassword']) ? htmlspecialchars($_POST['confirmPassword']) : false;
-                
-                if($password == $confirmPassword){
-                    $finalPassword = $this->security->hashPassword($password);
-                    $isRegister = $this->userModel->addUser($lastName,$firstName,$mail,$finalPassword);
-                }
-
-            }
-
-            parent::Render('App/View/registerView.php',array('isRegister'=>$isRegister,
-                                                             'confirmPassword'=>$confirmPassword
-                                                            ));
-        }   
+            parent::Render('App/View/index.php',array('user'=>$getUserInfo));
+        }
     } 
+?>

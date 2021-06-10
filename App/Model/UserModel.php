2ac4a4327addb0f265efae256bc4419a11fde004
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-06-10 18:18:14 +0200

    add ,  on getUserInfo() method

--- App/Model/UserModel.php
+++ App/Model/UserModel.php
@@ -3,7 +3,7 @@
     use App\Model\Model; 
 
     class UserModel extends Model{
-        public function getUserInfo(){
+        public function getUserInfo($mail,$password){
             $getUserInfo = parent::SelectFilter(array('lastName','firstName','mail','password','token','isAdmin'),'user',"mail = 'abdessamad.bannouf@laposte.net'");
 
             return $getUserInfo;

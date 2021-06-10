e79c34934ccd52dad980b986f9f626323111633f
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-06-10 18:18:56 +0200

    Add adminView Page

---
+++ App/View/AdminAddView.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin</title>
+        <meta charset="UTF-8"/>
+        <!-- Bootstrap Core CSS -->
+        <link href="../App/Public/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <script src="../App/Public/template/vendor/jquery/jquery.min.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+        <form>
+            <input type="text" name="title">
+            <input type="text" name="chapo">
+            <input type="text" name="image">
+            <input type="text" name="content">
+            <input type="submit">
+        </form>
+    </body>
+</html>

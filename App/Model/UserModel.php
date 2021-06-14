1db0e1d63f4bad9b36f0720bc49f9054c44ee9f6
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-06-14 12:33:29 +0200

    implements addPost(), updatePost(), deletePost() method

--- App/Model/UserModel.php
+++ App/Model/UserModel.php
@@ -8,5 +8,17 @@
 
             return $getUserInfo;
         }
+
+        public function addPost($title,$chapo,$image,$content,$author,$date){
+            $addPost = parent::RequestInsert('post',array('title','chapo','image','content','author','date','isValid'),array($title,$chapo,$image,$content,$author,$date,0));
+        }
+
+        public function updatePost($title,$chapo,$image,$content,$author,$date){
+            $updatePost = parent::RequestModify('post',array('title','chapo','image','content','author','date','isValid'),array($title,$chapo,$image,$content,$author,$date,0),'post_id',9);
+        }
+
+        public function deletePost($id){
+            $deletePost = parent::RequestDelete('post','post_id',$id);
+        }
     }
 ?>

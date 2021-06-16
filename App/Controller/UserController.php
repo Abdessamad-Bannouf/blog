7e22c7c38139882d1871a1a61e094084512825be
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-06-16 17:45:33 +0200

    Fix UserController branch

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -8,79 +8,29 @@
 
     use App\Classes\Form\Form;
 
-    use App\Classes\Date\Date;
-
-    use App\Classes\Security\Security;
-
     class UserController extends Controller{
         private $userModel;
         private $session;
         private $form;
-        private $security;
-        private $date;
-
 
         public function __construct(){
             $this->userModel = new userModel;
 
+            $this->session = new Session(array("lastName","firstName"),array("Bannouf","Abdessamad"));
+
             $this->form = new Form;
 
-            $this->date = Date::GetDate();
-
-            $this->security = new Security;
+            $this->session->GetSession();
         } 
 
-        public function login(){
-            if(isset($_POST['mail']) AND isset($_POST['password'])){
-                
-                $mail = htmlspecialchars($_POST['mail']); 
-                $password = htmlspecialchars($_POST['password']);
-
-                $getUserInfo = $this->userModel->getUserInfo($mail,$password);
-                $getUserInfo = $getUserInfo->fetchAll();
-                
-                //Ajouter la verif password-verify
-                var_dump($this->security->decryptPassword($password,$getUserInfo[0]['password'])); die();
-
-                $this->session = new Session(array("lastName","firstName", "user_id"),array($getUserInfo[0]['lastName'],$getUserInfo[0]['firstName'],$getUserInfo[0][5]));
-                $this->session->GetSession();
-
-                if($getUserInfo[0]['isAdmin'])
-                    header('location: '. WebSiteLink.'User/admin');
-
-                    else
-                        $this->home($getUserInfo[0]);
-            }
-
-            parent::Render('App/View/loginView.php',array());
+        public function home(){ 
+            /*if(isset($_POST['mail'])){
+                $mail = htmlspecialchars($_POST['mail']);
+            }*/
+            
+            $getUserInfo = $this->userModel->getUserInfo(); 
+             
+            parent::Render('App/View/index.php',array('user'=>$getUserInfo));  
         } 
-
-        public function register(){
-            $confirmPassword = null;
-            $isRegister = null;
-
-            if(isset($_POST['lastName']) AND isset($_POST['firstName']) AND isset($_POST['mail']) AND isset($_POST['password']) AND isset($_POST['confirmPassword'])){
-                $lastName = $_POST['lastName'];
-                $firstName = $_POST['firstName'];
-                $mail = htmlspecialchars($_POST['mail']); 
-                $password = htmlspecialchars($_POST['password']);
-                $confirmPassword = isset($_POST['confirmPassword']) ? htmlspecialchars($_POST['confirmPassword']) : false;
-                
-                if($password == $confirmPassword){
-                    $finalPassword = $this->security->hashPassword($password);
-                    $isRegister = $this->userModel->addUser($lastName,$firstName,$mail,$finalPassword);
-                }
-            }
-
-            parent::Render('App/View/registerView.php',array('isRegister'=>$isRegister,
-                                                             'confirmPassword'=>$confirmPassword
-                                                            ));
-        }   
-
-        public function home($userData = false){ 
-            var_dump($userData);die();
-                         
-            parent::Render('App/View/index.php',array('user'=>$userData));  
-        }    
-    } 
+    }   
 ?>

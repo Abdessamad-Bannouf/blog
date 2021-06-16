8c3f420c1485172cc549613f198c3ada537a0c72
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-06-16 23:08:20 +0200

    Add password_verify filter + redirection for the user

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -36,20 +36,22 @@
                 $mail = htmlspecialchars($_POST['mail']); 
                 $password = htmlspecialchars($_POST['password']);
 
-                $getUserInfo = $this->userModel->getUserInfo($mail,$password);
+                $getUserInfo = $this->userModel->getUserInfo($mail);
                 $getUserInfo = $getUserInfo->fetchAll();
                 
                 //Ajouter la verif password-verify
-                var_dump($this->security->decryptPassword($password,$getUserInfo[0]['password'])); die();
-var_dump($getUserInfo);
-                $this->session = new Session(array("lastName","firstName", "user_id"),array($getUserInfo[0]['lastName'],$getUserInfo[0]['firstName'],$getUserInfo[0][5]));
-                $this->session->GetSession();
+                $passwordVerified = $this->security->decryptPassword($password,$getUserInfo[0]['password']);
 
-                if($getUserInfo[0]['isAdmin'])
+                if($passwordVerified){
+                    $this->session = new Session(array("lastName","firstName","mail","user_id"),array($getUserInfo[0]['lastName'],$getUserInfo[0]['firstName'],$getUserInfo[0]['mail'],$getUserInfo[0][5]));
+                    $this->session->GetSession();
+                
+                    if($getUserInfo[0]['isAdmin'])
                     header('location: '. WebSiteLink.'User/admin');
 
-                    else
-                        $this->home($getUserInfo[0]);
+                        else
+                            return $this->home();                
+                }
             }
 
             parent::Render('App/View/loginView.php',array());
@@ -77,12 +79,34 @@ var_dump($getUserInfo);
                                                             ));
         }   
 
-        public function home($userData = false){ 
-            var_dump($userData);die();
-                         
-            parent::Render('App/View/index.php',array('user'=>$userData));  
+        public function home(){ 
+            $getUserInfo = $this->userModel->getUserInfo($_SESSION['mail']);
+
+            parent::Render('App/View/index.php',array('user'=>$getUserInfo));  
         }    
 
+        public function sendMail(){
+
+            if(isset($_POST['send'])){
+            // Check for empty fields
+                if(!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['phone']) AND !empty($_POST['message']) AND filter_var($_POST['email'],FILTER_VALIDATE_EMAIL) != false)
+                {
+                    $name = strip_tags(htmlspecialchars($_POST['name']));
+                    $email_address = strip_tags(htmlspecialchars($_POST['email']));
+                    $phone = strip_tags(htmlspecialchars($_POST['phone']));
+                    $message = strip_tags(htmlspecialchars($_POST['message']));
+                        
+                    // Create the email and send the message
+                    $to = 'abdessamad.bannouf@laposte.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+                    $email_subject = "Website Contact Form:  $name";
+                    $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+                    $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+                    $headers .= "Reply-To: $email_address";	
+
+                    return true;	
+                }		
+            }
+        }
         public function admin(){    
             parent::Render('App/View/adminView.php',array());
         }

840c2b5fcc81a9ec043aa64b13325f7fc4859c60
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-06-24 16:41:57 +0200

    Add form in Admin Update view

--- App/View/AdminUpdateView.php
+++ App/View/AdminUpdateView.php
@@ -4,14 +4,71 @@
         <title>Admin</title>
         <meta charset="UTF-8"/>
         <!-- Bootstrap Core CSS -->
-        <link href="../App/Public/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
     </head>
 
     <body>
-        <a href="">
-            <button class="btn btn-success">
-                Modifier article
-            </button>
-        </a>
+        <div class="container-fluid">
+            <form method="post" action='<?= WebSiteLink ?>Admin/Update' enctype="multipart/form-data">            
+                <div class="container">
+                    <div class="formBox">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h1>Modification de post</h1>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="inputBox ">
+                                    <div class="inputText">Titre</div>
+                                    <input type="text" name="title" class="input">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6">
+                                <div class="inputBox">
+                                    <div class="inputText">Chapô</div>
+                                    <input type="text" name="chapo" class="input">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="inputBox">
+                                    <div class="inputText">Image</div>
+                                    <input type="file" name="image" class="input">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="inputBox">
+                                    <div class="inputText">Message</div>
+                                    <textarea class="input" name="content"></textarea>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <input type="submit" name="" class="button" value="Send Message">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     </body>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+
+        $(".input").focus(function() {
+            $(this).parent().addClass("focus");
+        })   
+        });
+    </script>
 </html>

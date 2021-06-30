89247899eb0f94be3c111f618d066b38f30ed87e
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-06-30 22:12:39 +0200

    Delete isValid and his value in sql request

--- App/Model/AdminModel.php
+++ App/Model/AdminModel.php
@@ -10,18 +10,16 @@
         }
 
         public function addPost($title,$chapo,$image,$content,$author,$date){
-            $addPost = parent::RequestInsert('post',array('title','chapo','image','content','author','date','isValid'),array($title,$chapo,$image,$content,$author,$date,0));
+            $addPost = parent::RequestInsert('post',array('title','chapo','image','content','author','date'),array($title,$chapo,$image,$content,$author,$date));
         }
 
         public function updatePost($id=false,$title=false,$chapo=false,$image=false,$content=false,$author=false,$date=false){
             if($title==false OR $chapo==false OR $image==false OR $content==false OR$author==false OR $date==false)
-                $updatePost = parent::SelectFilter(array('post_id','title','chapo','image','content','author','date','isValid'),'post','post_id='.$id.'');
+                $updatePost = parent::SelectFilter(array('post_id','title','chapo','image','content','author','date'),'post','post_id='.$id.'');
                 
                 else
-                    $updatePost = parent::RequestModify('post',array('title','chapo','image','content','author','date','isValid'),array($title,$chapo,$image,$content,$author,$date,0),'post_id',$id);
-            
-                
-              
+                    $updatePost = parent::RequestModify('post',array('title','chapo','image','content','author','date'),array($title,$chapo,$image,$content,$author,$date),'post_id',$id);
+                         
             return $updatePost;
         }
 

9ac2cdded98e897b2fbafd0658a36d9b9ad2c9ed
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-01 23:02:20 +0200

    Delete isValid on selectFilter Method

--- App/Model/PostModel.php
+++ App/Model/PostModel.php
@@ -4,7 +4,7 @@
 
     class PostModel extends Model{
         public function getPost(){
-            $post = parent::SelectFilter(array('title','chapo','image','content','author','date','isValid'),'post',false);
+            $post = parent::SelectFilter(array('title','chapo','image','content','author','date'),'post',false);
 
             return $post;
         }

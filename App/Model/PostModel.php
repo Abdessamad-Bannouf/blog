0720fb580576fcd890ccf18aa8ddf77039b611bc
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-01 23:02:51 +0200

    Fix PSR norm

--- App/Model/PostModel.php
+++ App/Model/PostModel.php
@@ -4,7 +4,7 @@
 
     class PostModel extends Model{
         public function getPost(){
-            $post = parent::SelectFilter(array('title','chapo','image','content','author','date'),'post',false);
+            $post = parent::selectFilter(array('title','chapo','image','content','author','date'),'post',false);
 
             return $post;
         }

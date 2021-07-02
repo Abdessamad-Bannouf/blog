0eafbeb99f92cb4568d632d6d2c3447ebf9fa9e5
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-02 02:20:51 +0200

    Fix sql join method

--- App/Model/Model.php
+++ App/Model/Model.php
@@ -49,7 +49,7 @@
 
 		protected function join($table,$alias1,$tableJoin,$aliasJoin,$id,$idJoin)
 		{
-			$sql = "SELECT * FROM ".$table." AS ".$alias1." JOIN ".$tableJoin." AS ".$aliasJoin." ON ".$alias1.".".$id."=".$aliasJoin.".".$idJoin. " WHERE ".$alias1.".".$id."=".$tableJoin."";
+			$sql = "SELECT * FROM ".$table." AS ".$alias1." JOIN ".$tableJoin." AS ".$aliasJoin." ON ".$alias1.".".$id."=".$aliasJoin.".".$idJoin. " WHERE ".$alias1.".".$id."=".$aliasJoin.".".$idJoin."";
 			var_dump($sql);
 			$join = $this->dbConnect()->query($sql);
 			

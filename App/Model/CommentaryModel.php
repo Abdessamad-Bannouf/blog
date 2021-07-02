b1b74fec69084cb1547068906ec7594d2a5a02c9
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-02 21:43:28 +0200

    Fix the filter value (commentary_id) on selectFilter() method

--- App/Model/CommentaryModel.php
+++ App/Model/CommentaryModel.php
@@ -11,7 +11,7 @@
         
         public function getCommentary($id = false){
             if($id != false)
-                $getCommentary = parent::selectFilter(array('commentary_id','content','date','user_id','post_id','is_valid'),'commentary','post_id="'.$id.'"');
+                $getCommentary = parent::selectFilter(array('commentary_id','content','date','user_id','post_id','is_valid'),'commentary','commentary_id="'.$id.'"');
                 
                 else
                     $getCommentary = parent::selectAll(array('commentary_id','content','date','user_id','post_id','is_valid'),'commentary');

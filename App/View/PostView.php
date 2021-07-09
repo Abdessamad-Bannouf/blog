f16deb83e716d3916e0d1d3b35235ee6fba6eca7
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-09 10:20:46 +0200

    Delete Nav section

--- App/View/PostView.php
+++ App/View/PostView.php
@@ -11,28 +11,9 @@
     <body>
         <div class="container">
             <div id="blog" class="row"> 
-                        
-                <div class="col-sm-2 paddingTop20">
-                    <nav class="nav-sidebar">
-                        <ul class="nav">
-                            <li class="active"><a href="javascript:;"><span class="glyphicon glyphicon-star"></span> News</a></li>
-                            <li><a href="javascript:;">Latest news</a></li>
-                            <li><a href="javascript:;">Updates</a></li>
-                            <li><a href="javascript:;">Training</a></li>
-                            <li><a href="javascript:;">Nutrition</a></li>
-                            <li><a href="javascript:;">Proteins</a></li>
-                            <li><a href="javascript:;">Recipes</a></li>
-                            <li><a href="javascript:;">Challenge</a></li>
-                            <li class="nav-divider"></li>
-                            <li><a href="javascript:;"><i class="glyphicon glyphicon-off"></i> Sign in</a></li>
-                        </ul>
-                    </nav>
-                        <div><h2 class="add">Place for your add!</h2></div>
-                </div>
-
-                <?php while ($donnees = $Data['post']->fetch()){ ?>
+                <?php while ($donnees = $data['post']->fetch()){ ?>
                 <div class="col-md-10 blogShort">
-                    <h1><?= $donnees['title'] ?></h1>
+                    <h1><a href="<?=  WebSiteLink; ?>post/show/<?= $donnees['post_id'] ?>"><?= $donnees['title'] ?></a></h1>
                     <h3><?= $donnees['chapo'] ?></h3>
                     <img src="../App/Public/img/<?= $donnees['image'] ?>" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
                     <article><p>
@@ -40,9 +21,7 @@
                     </p></article>
                     <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Lire plus</a> 
                 </div>
-                <?php } ?>
-                        
-                        
+                <?php } ?>                         
                     <div class="col-md-12 gap10"></div>
             </div>
         </div>

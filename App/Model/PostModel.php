c98c79c5e1c64bea603279d2b090e7e0558bd514
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-09 10:22:00 +0200

    Add join between commentary and user

--- App/Model/PostModel.php
+++ App/Model/PostModel.php
@@ -10,8 +10,8 @@
 
                 else
                     //$post = parent::join(array('post.post_id','post.title','post.chapo','post.image','post.content','post.author','post.date','commentary.commentary_id','commentary.content','commentary.date','commentary.user_id','commentary.post_id','commentary.is_valid'),'post','post','commentary','commentary','post_id','post_id',$id);
-                    //$post = parent::requestCustom("SELECT DISTINCT post.post_id,post.title,post.chapo,post.image,post.content,post.author,post.date FROM post AS post JOIN commentary AS commentary ON post.post_id=commentary.post_id WHERE post.post_id='$id'");
-                    $post = parent::selectFilter(array('post_id','title','chapo','image','content','author','date'),'post',"post_id='$id'");
+                    $post = parent::requestCustom("SELECT DISTINCT (post.post_id),post.title,post.chapo,post.image,post.content,post.author,post.date,commentary.commentary_id, commentary.content, commentary.date,commentary.user_id,commentary.post_id,commentary.is_valid, user.firstname FROM post AS post JOIN commentary AS commentary ON post.post_id=commentary.post_id JOIN user AS user ON commentary.user_id=user.user_id WHERE post.post_id='$id'");
+                    //$post = parent::selectFilter(array('post_id','title','chapo','image','content','author','date'),'post',"post_id='$id'");
 
             return $post;
         }

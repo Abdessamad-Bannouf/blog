11f2833e5c0b8331c586437f2b40db4e7b88a985
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-22 11:43:59 +0200

    Delete var_dump() & die()

--- App/Model/AdminModel.php
+++ App/Model/AdminModel.php
@@ -14,14 +14,11 @@
         }
 
         public function updatePost($id=false,$title=false,$chapo=false,$image=false,$content=false,$author=false,$date=false){
-            var_dump(array($title,$chapo,$image,$content,$author,$date));
-            var_dump($id);
             if($id != false)
                 $updatePost = parent::SelectFilter(array('post_id','title','chapo','image','content','author','date'),'post','post_id='.$id.'');
                 
                 else{
                     $updatePost = parent::RequestModify('post',array('title','chapo','image','content','author','date'),array($title,$chapo,$image,$content,$author,$date),'post_id',$id);
-                     die(array($title,$chapo,$image,$content,$author,$date));
                 }
             return $updatePost;
         }

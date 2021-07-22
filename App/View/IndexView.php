7f339a5979caf857a5dc5bdd04ffd07a92d09350
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-22 17:39:03 +0200

    Delete while(data) loop

--- App/View/IndexView.php
+++ App/View/IndexView.php
@@ -59,6 +59,19 @@
                     <li class="page-scroll">
                         <a href="#contact">Contact</a>
                     </li>
+                    <?php
+                    if(isset($_SESSION['firstName'])){ ?>
+                        <li class="page-scroll">
+                        <a href="<?= WebSiteLink; ?>user/logout">deconnexion</a>
+                        </li>
+                <?php }
+                        else{ ?>
+                            <li class="page-scroll">
+                                <a href="<?= WebSiteLink; ?>user/login">connexion</a>
+                            </li>
+                        
+                        <?php }
+                ?>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
@@ -68,15 +81,13 @@
 
     <!-- Header -->
     <header>
-<?php 
-    while($donnees = $data['user']->fetch()){ 
-?>
+
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <img class="img-responsive" src="img/profile.png" alt="">
                     <div class="intro-text">
-                        <span class="name"><?= $donnees['firstName'].' '.$donnees['lastName']; ?></span>
+                        <span class="name">BANNOUF Abdessamad</span>
                         <hr class="star-light">
                         <img src="../App/Public/file/moi.jpg" alt="Avatar" class="avatar">
                         <span class="skills">Développeur web</span> 
@@ -92,9 +103,7 @@
                 </div>
             </div>
         </div>
-<?php 
-    } 
-?>
+
     </header>
 
     <!-- Portfolio Grid Section -->
@@ -251,7 +260,57 @@
         </div>
     </section>
 
-    <?php require 'App/View/Footer.php'; ?>
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col col-md-4">
+                        <h3>Location</h3>
+                        <p>3 Avenue des Ajoncs
+                            <br>13800 Istres</p>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>Around the Web</h3>
+                        <ul class="list-inline">
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer-col col-md-4">
+                        <h3>A propos du Freelancer</h3>
+                        <p>Freelance est un template gratuit et open source crée par <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <?php if(isset($_SESSION['isAdmin'])){ ?>
+                        <a href="<?= WebSiteLink;?>admin/index">Administration</a>
+                    <?php } ?>
+                    <div class="col-lg-12">
+                        Copyright &copy; Blog 2021
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
     <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
         <a class="btn btn-primary" href="#page-top">

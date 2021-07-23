6dffc208619940a161aabb8d995486a4e964478f
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-23 10:20:45 +0200

    Add post view file

---
+++ App/View/PostView.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <link rel="stylesheet" href="../App/Public/css/post.css" />
+        <!------ Include the above in your HEAD tag ---------->
+    </head>
+
+    <body>
+        <?php require 'App/View/NavBar.php'; ?>
+        <div class="container">
+            <div id="blog" class="row"> 
+                <?php while ($donnees = $data['post']->fetch()){ ?>
+                <div class="col-md-10 blogShort">
+                    <h1><a href="<?=  WebSiteLink; ?>post/show/<?= $donnees['post_id'] ?>"><?= $donnees['title'] ?></a></h1>
+                    <h3><?= $donnees['chapo'] ?></h3>
+                    <img src="../App/Public/img/<?= $donnees['image'] ?>" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
+                    <article><p>
+                         <?= $donnees['content'] ?>  
+                    </p></article>
+                    <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Lire plus</a> 
+                </div>
+                <?php } ?>                         
+                    <div class="col-md-12 gap10"></div>
+            </div>
+        </div>
+        <?php require 'App/View/Footer.php'; ?>
+    </body>
+</html>

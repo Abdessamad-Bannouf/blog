73cedfaab497f6cbd6ea14c7b96e532f48fc1532
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2021-07-25 23:40:22 +0200

    Add wrong id when user failed the auth

--- App/View/loginView.php
+++ App/View/loginView.php
@@ -14,10 +14,13 @@
                 <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
         	        <div class="panel panel-default">
         		        <div class="panel-heading">
-                            <h3 class="panel-title">Please sign up for Bootsnipp <small>It's free!</small></h3>
+                            <h3 class="panel-title">Connectez-vous</h3>
                         </div>
                         <div class="panel-body">
                             <form role="form" method="post" action="<?= WebSiteLink; ?>/User/login">
+                                <?php if($data['mail'] == 1 OR $data['password'] == 1){ ?>
+                                    <p>mauvais identifiants</p>    
+                                <?php } ?>
                                 <div class="form-group">
                                     <input type="email" name="mail" id="email" class="form-control input-sm" placeholder="Email Address">
                                 </div>
@@ -27,7 +30,11 @@
     
                                 </div>
                                 
-                                <input type="submit" value="Register" class="btn btn-info btn-block">
+                                <div class="form-group">
+                                    <a href="http://localhost/Blog/User/register">Pas de compte ? S'enregistrer</a>
+                                </div>
+                                
+                                <input type="submit" value="Se connecter" class="btn btn-info btn-block">
                             
                             </form>
                         </div>
